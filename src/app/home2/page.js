b582e54063955d369c792fb2cdde31306032696a
Your Name <you@example.com>
2023-07-28 23:57:10 +0530

    #

--- src/app/home2/page.js
+++ src/app/home2/page.js
@@ -2,6 +2,7 @@ import React from 'react';
 const home2 = () => {
   return (
     <>
+    <h1>heelo</h1>
        <h2>Introducing our upcoming templates: these innovative designs are set to revolutionize the web. With cutting-edge features and stunning aesthetics, our templates will elevate your online presence. Stay tuned for a seamless user experience, intuitive navigation, and customizable options. Get ready to make a bold statement with our upcoming templates.</h2>
     </>
   );
